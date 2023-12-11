36768767bc237e7fe4ab42b24e16a090e332fd4a
Preston <preston.ramrattan@gmail.com>
2023-12-11 14:37:10 +0900

    finished project with ErrorKey except

--- backn.py
+++ backn.py
@@ -3,17 +3,13 @@ import requests
 
 API_key = "132c526a5209b2140d5d8ce975b57b4f"
 
-def get_data(place, days=None, option=None):
+def get_data(place, days=None):
     url =f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_key}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data['list']
     nr_value = 8 * days
     filtered_data = filtered_data[0:nr_value]
-    if option == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if option == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 if __name__=="__main__":
--- forecast.py
+++ forecast.py
@@ -1,14 +1,31 @@
 import streamlit as st
 import plotly.express as px
 from backn import get_data
-st.title("Weather Forecast for the Next Days")
+
+st.title("Weather Forecast")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select how many days of forecast you need.")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place.title()}")
 
-data = get_data(place, days, option)
+try:
+    if place:
 
+        filtered_data = get_data(place, days)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperatures (C)"})
-st.plotly_chart(figure)
+        if option == "Temperature":
+            temperatures = [float(dict["main"]["temp"]) / 10 for dict in filtered_data]
+            print(temperatures)
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperatures (C)"})
+            st.plotly_chart(figure)
+
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            st.image(image_paths, width=115)
+
+except KeyError:
+        st.subheader(f'...Sorry, it seems that "{place}" is not a city or country, please double check.')
